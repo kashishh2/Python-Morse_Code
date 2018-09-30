60bbf7ed0e85c70ed4b15aee47373975fa206e85
kashishh2 <kashishh2@gmail.com>
2018-09-30 16:38:01 +0530

    working proto

--- morse.py
+++ morse.py
@@ -22,7 +22,41 @@ def encrypt(message):
     cipher = ''
     for letter in message:
         if letter != ' ':
-            cipher += MORSE_CODE_DICT[letter] + ' '
+            cipher += morse[letter] + ' '
         else:
             cipher += ' '
     return cipher
+
+# a function to decrypt the morse code
+def decrypt(message):
+    message += ' '
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if (letter != ' '):
+            i = 0
+            citext += letter
+        else:
+            i += 1
+            if i == 2 :
+                decipher += ' '
+            else:
+                decipher += list(morse.keys())[list(morse
+                .values()).index(citext)]
+                citext = ''
+    return decipher
+
+if __name__ == '__main__':
+    print("\n Welcome to morse code translations \n Please select one function: \n 1. Encrypt to morse code \n 2. Decrypt to morse code")
+    opt = int(input("Enter your choice:"))
+    if opt == 1:
+        enc = str(input("Enter the String to be translated:"))
+        print("The morse code translation is :")
+        print(encrypt(enc))
+    elif opt == 2:
+        enc = str(input("Enter the string to be decripted:"))
+        print("The morse code translation is :")
+        print(decrypt(enc))
+    else:
+        print("Wrong option")
+        
